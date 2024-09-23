94254b651848da2f2240922fe236ab9c59cc3cc1
Stefan <stefancool00@gmail.com>
2024-09-23 07:13:19 -0700

    Q5 Done

--- 1_DuplicateInteger.py
+++ 1_DuplicateInteger.py
@@ -21,3 +21,30 @@ Learned:
 I should have used a Set instead of List for storing values. unique = set()
 O(nlogn) if you sort array and then loop through. Sorting is logn complexity
 """
+
+# Second solution 9/23/2024 - 2 mins:
+
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+
+        occur = set()
+
+        for i in nums:
+            if i in occur:
+                return True
+            else:
+                occur.add(i)
+        
+        return False
+    
+"""
+Time: O(n)
+Space: O(n)
+Attempts: 1
+Mistakes made:
+Forgot to return False after the loop
+
+
+Learned:
+
+"""
