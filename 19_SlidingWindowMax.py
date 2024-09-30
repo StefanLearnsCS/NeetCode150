17baad9709222ee42caa3baa17a02a2495803d23
Stefan <stefancool00@gmail.com>
2024-09-29 23:19:46 -0700

    Q19

--- 19_SlidingWindowMax.py
+++ 19_SlidingWindowMax.py
@@ -15,3 +15,28 @@ class Solution:
             output.append(maximum)
         
         return output
+    
+# Second solution 9/29/2024 - N/A - No idea how to do this..
+
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        output = []
+        q = collections.deque()
+        l = r = 0
+
+        while r < len(nums):
+
+            while q and nums[q[-1]] < nums[r]:
+                q.pop()
+            q.append(r)
+
+            if l > q[0]:
+                q.popleft()
+
+            if (r + 1) >= k:
+                output.append(nums[q[0]])
+                l += 1
+
+            r += 1
+
+        return output
