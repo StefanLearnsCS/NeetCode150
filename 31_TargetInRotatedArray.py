abdc4f05e8e6aaad22246e8d04210913646175bd
Stefan <stefancool00@gmail.com>
2024-10-07 22:25:39 -0700

    Q31 Done

--- 31_TargetInRotatedArray.py
+++ 31_TargetInRotatedArray.py
@@ -25,3 +25,29 @@ class Solution:
         return -1
 
 # Need to try again, this is close but not enough, need to rethink.
+
+# Second solution:
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        start, end = 0, len(nums) - 1
+        
+        while start <= end:
+            m = start + ((end - start) // 2)
+
+            if nums[m] == target:
+                return m
+
+            if nums[start] <= nums[m]:
+                if nums[start] <= target < nums[m]:
+                    end = m - 1
+                else:
+                    start = m + 1
+
+            else:
+                if nums[m] < target <= nums[end]:
+                    start = m + 1
+                else:
+                    end = m - 1
+
+        return -1
